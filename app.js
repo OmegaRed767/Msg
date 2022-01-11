da1daa198a496e326d3b81a4e273cc83eaae5634
OmegaRed767 <omegared767@gmail.com>
2022-01-11 12:01:06 +0530

    cors_error_handling

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+//cors
+const cors = require("cors");
 //bodyParser
 const bodyParser = require("body-parser");
 
@@ -25,12 +27,24 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+//cors
+app.use(cors());
 // path
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/Order", Order);
 app.use("/Product", Product);
 app.use("/Otp", Otp);
+//handling error
+app.use((req, res, next) => {
+  const error = new Error("Uh Oh Error occured");
+  error.status = 400;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status);
+  res.json({ msg: error.message });
+});
 
 module.exports = app;
