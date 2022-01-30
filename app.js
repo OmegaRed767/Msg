25957c4b5e5373c59983842ff22207923d02a3f9
OmegaRed767 <omegared767@gmail.com>
2022-01-30 19:58:22 +0530

    revert_app.js

--- app.js
+++ app.js
@@ -1,106 +1,53 @@
 const express = require("express");
 const app = express();
-//mongoose
-const mongoose = require("mongoose");
 //cors
 const cors = require("cors");
-// bodyparser
+//bodyParser
 const bodyParser = require("body-parser");
-//routes
-// const product = require("./api/routes/Products");
-// const orders = require("./api/routes/Orders");
-// const user = require("./api/routes/User");
-//connect to db
+
+// routes
+const Otp = require("./Api/Routes/Otp");
+const Chat = require("./Api/Routes/Chat");
+
+//mongoose
+const mongoose = require("mongoose");
+
 mongoose
   .connect(
-    "mongodb+srv://Kona:Persist@cluster0.cyiwc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
+    "mongodb+srv://Kona:" +
+      "Persist" +
+      "@cluster0.cyiwc.mongodb.net/MessageApp?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   )
   .then(() => {
     console.log("connected");
   })
-  .catch((x) => {
-    console.log(x);
+  .catch((err) => {
+    console.log(err);
   });
 //cors
 app.use(cors());
-//making folder available public
-app.use("/Upload", express.static("Upload"));
-//parse-body-request
+// path
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//handing path
 app.use("/msg", (req, res, next) => {
-  res.status(200).json({ msg: "Hello world23" });
+  res.status(200).json({ msg: "hello world" });
 });
-// app.use("/Product", product);
-// app.use("/Order", orders);
-// app.use("/User", user);
-
-//err
+app.use("/Otp", Otp);
+app.use("/Chat", Chat);
+//handling error
 app.use((req, res, next) => {
-  const error = new Error("Error Occured");
-  error.status = 404;
+  const error = new Error("Uh Oh Error occured");
+  error.status = 400;
   next(error);
 });
+
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    msg: error.message,
-  });
+  res.status(error.status);
+  res.json({ msg: error.message });
 });
+
 module.exports = app;
-
-// const express = require("express");
-// const app = express();
-// //cors
-// const cors = require("cors");
-// //bodyParser
-// const bodyParser = require("body-parser");
-
-// // routes
-// const Otp = require("./Api/Routes/Otp");
-// const Chat = require("./Api/Routes/Chat");
-
-// //mongoose
-// const mongoose = require("mongoose");
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://Kona:" +
-//       "Persist" +
-//       "@cluster0.cyiwc.mongodb.net/MessageApp?retryWrites=true&w=majority",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("connected");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-// //cors
-// app.use(cors());
-// // path
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use("/msg", (req, res, next) => {
-//   res.status(200).json({ msg: "hello world" });
-// });
-// app.use("/Otp", Otp);
-// app.use("/Chat", Chat);
-// //handling error
-// app.use((req, res, next) => {
-//   const error = new Error("Uh Oh Error occured");
-//   error.status = 400;
-//   next(error);
-// });
-
-// app.use((error, req, res, next) => {
-//   res.status(error.status);
-//   res.json({ msg: error.message });
-// });
-
-// module.exports = app;
